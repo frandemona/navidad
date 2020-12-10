edecd627fff8dd18b44e701f8ba943e541b43b1e
Francisco <franciscodemona@gmail.com>
2020-12-10 10:05:55 -0300

    Sumamos envío de Correo por QR

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-var QRCode = require('qrcode')
+const QRCode = require('qrcode')
+const nodemailer = require('nodemailer');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -26,20 +27,67 @@ exports.createQr = functions.firestore.document('/attendees/{documentId}').onCre
         await snap.ref.set({qr}, {merge: true});
         functions.logger.log('Created QR For', context.params.documentId);
       } catch (error) {
-        functions.logger.log('Error Creating QR For', context.params.documentId, 'Error',error);
+        functions.logger.error('Error Creating QR For', context.params.documentId, 'Error',error);
       }
     }
   })
 });
 
 // Send QR
-// exports.sendQr = functions.firestore.document('/attendees/{documentId}').onUpdate((snap, context) => {
-//   // Grab the current value of what was written to Cloud Firestore.
-//   const whatsapp = snap.data().whatsapp;
-
-//   // Access the parameter `{documentId}` with `context.params`
-//   functions.logger.log('Sending Whatsapp To', context.params.documentId, whatsapp);
+exports.sendQr = functions.firestore.document('/attendees/{documentId}').onUpdate(async (change, context) => {
+  const { email: emailBefore, qr: qrBefore } = change.before.data();
+  const { email, qr, firstName } = change.after.data();
   
-//   // const uppercase = original.toUpperCase();
-//   // return snap.ref.set({uppercase}, {merge: true});
-// });
+  if(emailBefore === email && qrBefore === qr) {
+    functions.logger.log('Document', context.params.documentId, 'changed but email and qr is the same');
+    return
+  } else if (emailBefore !== email && !qr) {
+    functions.logger.log('Document', context.params.documentId, 'changed but qr doesn\'t exist');
+    return
+  }
+
+  functions.logger.log('Sending Email To', email, 'for document', context.params.documentId);
+
+  const gmailEmail = functions.config().gmail.email;
+  const gmailPassword = functions.config().gmail.password;
+  const mailTransport = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: gmailEmail,
+      pass: gmailPassword,
+    },
+  });
+  const mailOptions = {
+    from: 'Navidad <navidadcbac1@gmail.com>',
+    to: email,
+    subject: 'Navidad QR',
+    text: `Hola ${firstName || ''}! Acá está el QR para utilizar adjuntado en este correo. Es único e intransferible.`,
+    html: `<p>Hola ${firstName || ''}!</p><p>Acá está el QR para utilizar. Es único e intransferible.</p><br /><br /><img src="cid:qr@nodemailer.com" alt="QR Code"/>`,
+    attachments: [
+      {
+        filename: 'QR-Navidad.png',
+        path: qr,
+        cid: 'qr@nodemailer.com'
+      },
+      {
+        filename: 'QR Navidad.png',
+        path: qr
+      },
+    ]
+  };
+
+  try {
+    await mailTransport.sendMail(mailOptions);
+    functions.logger.log('QR Sent To', email, 'For', context.params.documentId,);
+    await change.after.ref.set({qrSent: true}, {merge: true});
+  } catch (error) {
+    functions.logger.error('Error Sending QR For', context.params.documentId, 'Error', error);
+  }
+
+  // const requestUrl = `https://api.budgetsms.net/testsms/username=${functions.config().budgetsms.username}&userid=${functions.config().budgetsms.userid}&handle=${functions.config().budgetsms.handle}`
+  // https://api.budgetsms.net/sendsms/ - Produccion
+  // https://api.budgetsms.net/testsms/ - Test
+  
+  // const uppercase = original.toUpperCase();
+  // return snap.ref.set({uppercase}, {merge: true});
+});
